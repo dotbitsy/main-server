cc6c219a1bea005517721725acf69c0250d01313
Sina <sina.solouk@bitingbit.com>
2022-08-30 12:45:31 +0200

    TOTAL VALUE

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,23 +2,7 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get("hello") { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
-    }
     
-    app.get { req async throws in
-           try await req.view.render("institute", ["title": "Hello Vapor!"])
-       }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
-    
-    let bitsys = BitSystem(eventloop: app.eventLoopGroup.next())
-    app.webSocket("bitsys") { req, ws async in
-        await bitsys.connect(ws: ws)
-    }
-
     
     let chain = try BlockChain.query(on: app.db).all().wait()
     let stats =  StatsController(chain: chain)
@@ -27,4 +11,21 @@ func routes(_ app: Application) throws {
     try app.register(collection: BlockChainController(statsController: stats))
     try app.register(collection: BlockModelController())
     try app.register(collection: stats)
+    
+    let bitsys = BitSystem(eventloop: app.eventLoopGroup.next())
+    app.webSocket("bitsys") { req, ws async in
+        await bitsys.connect(ws: ws)
+    }
+
+    app.get("hello") { req in
+        return req.view.render("index", ["title": "Hello Vapor!"])
+    }
+    
+    app.get { req async throws in
+        try await req.view.render("institute", ["title": "Total: \(stats.totalChainValue)"])
+    }
+    
+    app.get("hello") { req -> String in
+        return "Hello, world!"
+    }
 }
